5911a8b60bb344a7e85eb7d4151c32539968e800
syobon-tech <syobon_tech@outlook.com>
2021-01-02 19:54:28 +0900

    fix: 読点の処理を変更

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,9 @@ use regex::Regex;
 pub fn translate(text: &str, bunmatsu: &str, nai_to_nyai: bool) -> String {
     let re1 = Regex::new(r"(.+?)(である|だ|です)(。|\(|\s|$)").unwrap();
     let re2 = Regex::new(r"([えけせてねへめれ])ます(。|\(|\s|$)").unwrap();
-    let re3 = Regex::new(r"(\S+?[^。\)\s])(。|\(|\s|$)").unwrap();
-    let re4 = Regex::new(r"、にゃ").unwrap();
-    let re5 = Regex::new(r"ないにゃ").unwrap();
-    let re6 = Regex::new(r"ない").unwrap();
+    let re3 = Regex::new(r"(\S+?[^、。\)\s])(。|\(|\s|$)").unwrap();
+    let re4 = Regex::new(r"ないにゃ").unwrap();
+    let re5 = Regex::new(r"ない").unwrap();
 
     let mut new: String;
     new = text.to_string();
@@ -16,10 +15,9 @@ pub fn translate(text: &str, bunmatsu: &str, nai_to_nyai: bool) -> String {
     new = re2.replace_all(&new, "$1る$2").to_mut().to_string();
     let replacement: &str = &format!("$1{}$2", bunmatsu);
     new = re3.replace_all(&new, replacement).to_mut().to_string();
-    new = re4.replace_all(&new, "、").to_mut().to_string();
     if nai_to_nyai {
+        new = re4.replace_all(&new, "にゃい").to_mut().to_string();
         new = re5.replace_all(&new, "にゃい").to_mut().to_string();
-        new = re6.replace_all(&new, "にゃい").to_mut().to_string();
     }
     return new;
 }
